03539b889b578cf4da47131a7b7545c0bd1c52f0
George Rye <jorge.centeno.ma@gmail.com>
2022-05-09 10:32:07 +0200

    integration with rail

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.demo.controllers.Ejercicio1;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	Ejercicio1 ejercicio;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void greetTest(){
+		String resultado = ejercicio.greet();
+		assertEquals("Bienvenido al servidor backend", resultado);
 	}
 
 }
