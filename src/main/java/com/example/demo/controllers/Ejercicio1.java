6946ec2f32e15272080d056310813455c734453a
George Rye <jorge.centeno.ma@gmail.com>
2022-05-09 13:59:14 +0200

    update

--- src/main/java/com/example/demo/controllers/Ejercicio1.java
+++ src/main/java/com/example/demo/controllers/Ejercicio1.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
